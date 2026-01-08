44eb65a8d6ce3fa4f9a82d399665452de4289d40
berkankaraveli <berkankaraveli@abv.bg>
2026-01-08 13:24:48 +0200

    Added new buttons below "Играй" , fixed the css and added photo for them.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,34 +1,41 @@
-// Home.jsx
+// src/components/Home.jsx
+
 import React from 'react';
-import AvatarCreator from './AvatarCreator.jsx'; // Увери се, че пътят е верен
+// Увери се, че AvatarCreator е импортиран правилно според твоята структура
+// Ако дава грешка, провери дали файлът е .jsx или .js и къде се намира
+import AvatarCreator from './AvatarCreator'; 
 import './Home.css';
 
 const Home = ({ user, avatar, onStartGame }) => {
-  // Изчисляване на процента за XP бара
   const xpPercentage = user.level > 0 ? (user.xp / (user.level * 200)) * 100 : 0;
 
   return (
     <div className="home-screen">
-      {/* ЛЯВО: БУТОН ИГРАЙ */}
+      {/* --- ЛЯВ ПАНЕЛ --- */}
       <div className="home-panel left-panel">
+        {/* Главен бутон ИГРАЙ */}
         <button className="big-play-btn" onClick={onStartGame}>
           ИГРАЙ
         </button>
+
+          <button className="big-play-btn">МАГАЗИН</button>
+          <button className="big-play-btn">ИНВЕНТАР</button>
+          <button className="big-play-btn">РОЛИ</button>
+          <button className="big-play-btn">ГИЛДИЯ</button>
+
       </div>
 
-      {/* ЦЕНТЪР: ГЕРОЙ КАРТА */}
+      {/* --- ЦЕНТРАЛЕН ПАНЕЛ (Аватар) --- */}
       <div className="home-panel center-panel">
-        {/* Подаваме обекта avatar към компонента */}
+         {/* Увери се, че този компонент съществува и работи */}
         <AvatarCreator selections={avatar} />
       </div>
 
-      {/* ДЯСНО: ПРОФИЛ */}
+      {/* --- ДЕСЕН ПАНЕЛ (Профил) --- */}
       <div className="home-panel right-panel">
         <div className="profile-card">
           <h3>{user.name}</h3>
           <div className="lvl-info">LEVEL {user.level}</div>
-          
-          {/* XP BAR */}
           <div className="xp-bar-outer">
             <div 
                 className="xp-bar-inner" 
@@ -37,11 +44,10 @@ const Home = ({ user, avatar, onStartGame }) => {
           </div>
           <p className="xp-text">{user.xp} / {user.level * 200} XP</p>
         </div>
-        
         <div className="missions-list">
           <h4>МИСИИ</h4>
           <div className="mission-item">Изиграй 1 игра (+50 XP)</div>
-           <div className="mission-item">Промени аватара (+20 XP)</div>
+          <div className="mission-item">Промени аватара (+20 XP)</div>
         </div>
       </div>
     </div>
